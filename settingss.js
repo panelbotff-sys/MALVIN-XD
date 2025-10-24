4ce39d5a18bd5858fd00307b167a059dd63a7842
panelbotff-sys <panel.botff@gmail.com>
2025-10-24 21:49:52 +0700

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -4,7 +4,7 @@ module.exports = {
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  PREFIX: "MANZZ HECKER", // prefix (e.g., ., /, !, *)
   
   TIMEZONE: "nyaMANZZ HECKER" //put your country timeZone....leave blank if u don't know.
 };
